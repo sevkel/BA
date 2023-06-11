df0e6a95c7b294cda696cd421c3930d9573470c5
sevkel <sevkeller@web.de>
2023-06-11 20:34:00 +0200

    changed input to while True loop for unlimited number of functionals and basis sets

--- make_ini2.0.py
+++ make_ini2.0.py
@@ -1,40 +1,54 @@
 import sys, os
 import configparser
 
-
 # check number of arguments as input
-if len(sys.argv) < 2:
-    print("Please insert the filename as argument.")
+if len(sys.argv) != 2:
+    print("Please insert the system- or molecule name as an argument.")
     sys.exit(1)
 
-# Define sections and parameters
+# Define sections and parameters for functionals and basis sets
 sections = {
-    'functionals': ['func1', 'func2', 'func3'],
-    'basis': ['basis1', 'basis2'],
-    'system': ['molecule']
+    'functionals': [],
+    'basis': [],
     # Add more sections and parameters if wanted
 }
 
 # create an instance of the configparser
 config = configparser.ConfigParser()
 
-
 # Add sections and parameters
 for section, params in sections.items():
     config.add_section(section)
-    for param in params:
-        value = input(f"Please put in a value for {param} in {section}: ")
-        config.set(section, param, value)
+    if section == 'functionals':
+        i = 1
+        while True:
+            param = 'functional' + str(i)
+            value = input(f"Please put in a value for {param} (or '*' to stop): ")
+            if value == '*':
+                break  # Exit the loop if '*' is entered
+            params.append(param)
+            config.set(section, param, value)
+            i += 1
+    elif section == 'basis':
+        i = 1
+        while True:
+            param = 'basis' + str(i)
+            value = input(f"Please put in a value for {param} (or '*' to stop): ")
+            if value == '*':
+                break  # Exit the loop if '*' is entered
+            params.append(param)
+            config.set(section, param, value)
+            i += 1
+config['system'] = {'molecule': sys.argv[1]}
 
-#define output folder
+# Define output folder
 system = config['system']['molecule']
 output_folder = './testsystems/' + system
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-
-# save INI-file
+# Save INI-file
 configfile_path = os.path.join(output_folder, system + '.ini')
 with open(configfile_path, 'w') as configfile:
     config.write(configfile)
