07aaa6e3405a904a3dfe7150df5bd9d92a514826
sevkel <sevkeller@web.de>
2023-06-14 11:46:41 +0200

    Added function to create and save xyz-file for optimized geometry

--- DFT_gen_2.0.py
+++ DFT_gen_2.0.py
@@ -20,6 +20,22 @@ def save_orbitals(folder_path, homo_index, lumo_index):
     np.savetxt(folder_path + '/' + 'lumo.txt', orbitals[:, lumo_index])
     np.savetxt(folder_path + '/' + 'lumo_plus.txt', orbitals[:, homo_index + 1])
 
+#saving optimized geometry as xyz-file
+def save_opt_xyz_format(mol, system, folder_path = ''): # added
+    coords = mol.atom_coords(unit='ang')
+    with open(folder_path + '/' + system + '_optimized.xyz', 'w') as xyz_file:
+        xyz_file.write(str(mol.natm) + '\n')
+        xyz_file.write(system + '\n')
+        j = 0
+        for atom in coords:
+            list = []
+            list.append(mol.atom_symbol(j))
+            for i in range(len(atom)):
+                list.append(atom[i])
+            s = '      '.join(str(x) for x in list)
+            j += 1
+            xyz_file.write(s+'\n')
+
 
 if __name__ == "__main__":
 
@@ -29,6 +45,7 @@ if __name__ == "__main__":
     config.read(sys.argv[2])
     functionals = config['functionals']
     basis_sets = config['basis']
+    system = config['system']['molecule'] #added
 
     # initialize molecule
     mol = gto.Mole()
@@ -64,20 +81,26 @@ if __name__ == "__main__":
 
             # SAVING DATA
 
-            output_folder = './testsystems/' + config['system']['molecule'] + '/' + functionals[functional] + '_' + \
+            # define destination folder
+            output_folder = './testsystems/' + system + '/' + functionals[functional] + '_' + \
                             basis_sets[basis] + '/results'
 
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)
 
+            # save optimized geometry as xyz-file
+            save_opt_xyz_format(mol_opt, system, output_folder) #added
+
             # save energies
             np.savetxt(output_folder + '/' + 'energies.txt', energies)
             homo_lumo_filename = 'homo_lumo_energies.txt'
             with open(output_folder + '/' + homo_lumo_filename, 'w') as file:
-                file.write("HOMO energy: {:.6f} Hartree\n".format(homo_energy))
+                file.write("HOMO energy: {:.6f} Hartree\n".format(homo_energy)) #check if its really Hartree
                 file.write("LUMO energy: {:.6f} Hartree\n".format(lumo_energy))
                 file.write("HOMO-LUMO gap: {:.6f} Hartree\n".format(homo_lumo_gap))
 
+            # saving cube files for homo and lumo orbtials
             save_cubefile(output_folder, homo_idx, lumo_idx)
             save_orbitals(output_folder, homo_idx, lumo_idx)
+
 
